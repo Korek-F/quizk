c71854d00a9cfd7630e6e9d8397f6783b4b275ac
unknown <filip.korycki2222@gmail.com>
2021-04-17 21:05:37 +0200

    Przygotowanie do wstawienia na heroku

--- quizk/settings.py
+++ quizk/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '9aa$@qr1mij%w)jf$w8ug6qc^c%^9f6wpp20p@adjk=5dw)!r)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "quizk.heroku.com"]
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
