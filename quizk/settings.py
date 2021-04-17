aa828ab90bc42c511612d895d4697b148b86c682
unknown <filip.korycki2222@gmail.com>
2021-04-17 21:10:32 +0200

    heroku

--- quizk/settings.py
+++ quizk/settings.py
@@ -123,6 +123,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 LOGOUT_REDIRECT_URL ='main_page'
 
 
